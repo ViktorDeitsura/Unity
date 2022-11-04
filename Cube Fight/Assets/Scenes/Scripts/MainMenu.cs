0091a652728822b2b6d4806e537a5e28e77c794e
Viktor Deitsura <ViktorDeitsura@gmail.com>
2022-11-04 19:42:04 +0700

    create project, add menu, add stage and stage level

---
+++ Cube Fight/Assets/Scenes/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void Start(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
